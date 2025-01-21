bcd4cfa7b8b899a546c59f179b82b4020584b9a7
Juan Felipe <juanfecazuluaga@gmail.com>
2025-01-20 20:33:08 -0500

    Add password

--- config/db.js
+++ config/db.js
@@ -15,7 +15,7 @@ const connectToDatabase = async () => {
 
             pool = mysql.createPool({
                 host: process.env.DB_HOST,
-                user: secret.username,
+                user: process.env.DB_USER,
                 password: process.env.DB_PASSWORD,
                 database: process.env.DBNAME,
                 port: process.env.DB_PORT,
